5cd91e40cf717eaa7b2052f16e9a6cae32cdcda4
Emmanuel Berchie <emmanuelberchie9@gmail.com>
2019-11-01 04:18:02 +0000

    adding bounce to ball

---
+++ Assets/Scripts/LoseColider.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoseColider : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        // only use when confident scene name wont change
+        SceneManager.LoadScene("Game Over");
+    }
+}

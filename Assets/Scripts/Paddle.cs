9be2c5c78059003fcd977d276a355fdf38c38f00
Emmanuel Berchie <emmanuelberchie9@gmail.com>
2019-11-05 19:33:24 +0000

    Changes to ball and created collisions

---
+++ Assets/Scripts/Paddle.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Paddle : MonoBehaviour
+{
+    // Configuration Parameters 
+
+    [SerializeField] float screenWidthInUnits = 16f;
+    [SerializeField] float screenMin = 1.05f;
+    [SerializeField] float screenMax = 14.99f;
+ 
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // find mouse position and then set it to within the screen width 
+        float mousePosInUnits = Input.mousePosition.x / Screen.width * screenWidthInUnits;
+        // Vector 2 is a compact way of storing x and y componenents, do not need y because it is a 2d game
+        Vector2 paddlePos = new Vector2(transform.position.x, transform.position.y);
+        paddlePos.x = Mathf.Clamp(mousePosInUnits, screenMin, screenMax);
+        transform.position = paddlePos;
+    }
+        
+
+    }
+
